1bc09e29e5862ac2574152aaa1837573f532eb53
Rita Uzoma <you@example.com>
2023-07-27 14:56:36 +0200

    completed the collection gallery page

--- component/Sidebar.js
+++ component/Sidebar.js
@@ -1,29 +1,69 @@
+"use client";
+
 import React from "react";
+import styled from "styled-components";
 
 export const Sidebar = () => {
   return (
-    <div className="sidebar" style={{ border: "2px solid green", flex: "11" }}>
+    <div className="sidebar" style={{flex: "11" }}>
       <div>
-        <h4>Product Type</h4>
-        <p style={{ border: "2px solid green" }}>
-          Furniture jhfytfgfdsjyfdtshdf
-        </p>
-        <p>Homeware</p>
-        <p>Sofas</p>
-        <p>Light Fittings</p>
-        <p>Accessories</p>
+        <h4
+          style={{
+            marginBottom: "15px",
+            fontStyle: "normal",
+            fontWeight: "400",
+          }}
+        >
+          Product Type
+        </h4>
+        <Paragraph>
+          <p>= Furniture</p>
+          <p>= Homeware</p>
+          <p>= Sofas</p>
+          <p>= Light Fittings</p>
+          <p>= Accessories</p>
+        </Paragraph>
       </div>
 
-      <h4>Price</h4>
-      <p>0 - 100</p>
-      <p>101 - 250</p>
-      <p>250+</p>
+      <h4
+        style={{
+          marginTop: "50px",
+          marginBottom: "15px",
+          fontStyle: "normal",
+          fontWeight: "400",
+        }}
+      >
+        Price
+      </h4>
+      <Paragraph>
+        <p>= 0 - 100</p>
+        <p>= 101 - 250</p>
+        <p>= 250+</p>
+      </Paragraph>
 
-      <h4>Designer</h4>
-      <p>Robert Smith</p>
-      <p>Liam Galagher</p>
-      <p>Biggie Smalls</p>
-      <p>Thom Yorke</p>
+      <h4
+        style={{
+          marginTop: "50px",
+          marginBottom: "15px",
+          fontStyle: "normal",
+          fontWeight: "400",
+        }}
+      >
+        Designer
+      </h4>
+      <Paragraph>
+        <p>= Robert Smith</p>
+        <p>= Liam Galagher</p>
+        <p>= Biggie Smalls</p>
+        <p>= Thom Yorke</p>
+      </Paragraph>
     </div>
   );
 };
+
+const Paragraph = styled.div`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 22px;
+`;
